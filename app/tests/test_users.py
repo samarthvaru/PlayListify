0a782ca4db945a8b5ba575c850833e3c889cb313
Samarth Varu <samarthvaru111@gmail.com>
2024-02-28 15:05:38 -0500

    functionality 2- added inital Tests and setup

---
+++ app/tests/test_users.py
@@ -0,0 +1,34 @@
+import pytest
+from app.users.models import User
+from app import db
+
+@pytest.fixture(scope='module')
+def setup():
+    #setup
+    session = db.get_session()
+    yield session
+    #teardown
+    q = User.objects.filter(email= 'test@test.com')
+    if q.count() != 0:
+        q.delete()
+    session.shutdown()
+    
+
+def test_create_user(setup):
+    User.create_user(email= 'test@test.com', password='abc123')
+    
+def test_duplicate_user(setup):
+    with pytest.raises(Exception):
+        User.create_user(email= 'test@test.com', password='abc123')
+        
+def test_invalid_email(setup):
+    with pytest.raises(Exception):
+        User.create_user(email= 'test@test', password='abc123')
+
+def test_valid_password(setup):
+    q = User.objects.filter(email = 'test@test.com')
+    assert q.count() == 1
+    
+    user_obj = q.first()
+    assert user_obj.verify_password('abc123') == True
+    assert user_obj.verify_password('abc1234') == False
